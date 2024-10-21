0f7b58c2634536f65c31562414ee7e2fe38c7d5d
Daniel-Platz <dani0005@stud.kea.dk>
2024-10-21 09:08:49 +0200

    third commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello Europe");
         System.out.println("Hello Class");
+        System.out.println("Hello Person");
     }
 }
